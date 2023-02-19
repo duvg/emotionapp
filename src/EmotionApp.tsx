e0850b121649496c675687106d2f7ed0fef1c7fb
Duviel Garcia <duviel7@gmail.com>
2023-02-18 21:03:52 -0500

    First commit

---
+++ src/EmotionApp.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EmotionApp = () => {
+  return <div>EmotionApp</div>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { EmotionApp } from './EmotionApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <EmotionApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
